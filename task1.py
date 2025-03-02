978ee54e8e6d9b8b7f9672195f250f3201e940be
ALISHAMATHUR007 <mathuralisha7002@gmail.com>
2025-03-02 22:49:10 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+n=int(input("enter the first number:"))
+p=int(input("enter the second number:"))
+m=n+p
+l=n-p
+o=n*p
+r=n/p
+print("Addition:",m)
+print("subtract:",l)
+print("multiplication:",o)
+print("Divions:",r)
