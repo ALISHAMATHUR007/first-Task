9109a8b849064e50881faf8e1a873206b5a9ca1e
ALISHAMATHUR007 <mathuralisha7002@gmail.com>
2025-03-02 23:23:38 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,3 +1,11 @@
+#  ----------------task2------------------
+h=input("enter the first name:")
+g=input("enter the last name:")
+b= h+g
+print(b)
+print("hello", b,",welcome to my code")
+
+---------task2------------
 n=int(input("enter the first number:"))
 p=int(input("enter the second number:"))
 m=n+p
